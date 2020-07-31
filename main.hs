4d372d2a2df33a22f9219c38f2b88f80cf3244ca
martin.velez1 <martinvelezf@hotmail.com>
2020-07-31 14:31:45 -0500

    check road time

--- main.hs
+++ main.hs
@@ -14,3 +14,14 @@ covertDatetoDay date=x
         tls=map (read::String->Int) $tail arraydate
         hd=(read::String->Integer) (head arraydate)
         x=toWeekDate (fromGregorian hd (tls!!0) (tls!!1) )
+
+
+
+checktime time=x
+    where 
+        arraytime=map (read::String->Int)(splitOn ":" time)
+        x=timeRoad  $((arraytime!!0)*100)+(arraytime!!1) 
+        
+timeRoad x
+    | ((x>=700) && (x<=930)) || ((x>=1600) && (x<=1930)) =False    
+    |otherwise = True
